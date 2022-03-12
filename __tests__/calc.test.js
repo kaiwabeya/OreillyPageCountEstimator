80d2fcf84b1583ed09463be778839240e2181997
Yu Nakata <yu.nakata@tier4.jp>
2022-03-12 19:07:17 +0900

    change: 再帰構造に変更

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -18,8 +18,8 @@ describe("test suite", () => {
         result = main.calc(content);
         expect(result).toEqual(
             [
-                { "title": "Chapter 1", "pages": "-0.0", "chars": -20 },
-                { "title": "1.1 hoge", "pages": "0.1", "chars": 170, "level": 1 },
+                { "title": "Chapter 1", "pages": "0.1", "chars": 140, "level": 0},
+                { "title": "1.1 hoge", "pages": "0.0", "chars": 10, "level": 1 },
                 { "title": "1.1.1 Hoge", "pages": "0.0", "chars": 50, "level": 2 },
                 { "title": "1.1.2 Huga hoge", "pages": "0.1", "chars": 110, "level": 2 }
             ]
